6bdaf36ff2ce764e5e6be6ad7d09f82bd2793462
AleP04 <alessandropasqui90@gmail.com>
2021-03-22 12:11:34 +0100

    First Commit

---
+++ src/linetools/Point.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linetools;
+
+/**
+ *
+ * @author alep
+ */
+public class Point {
+
+    private final int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double distance(Point b) {
+        return Math.sqrt(Math.pow(this.x - b.x, 2) + Math.pow(this.y - b.y, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" + "x=" + x + ", y=" + y + '}';
+    }
+
+    public Point midpoint(Point b) {
+        double xM = (double)(this.x + b.x) / 2;
+        double yM = (double)(this.y + b.y) / 2;
+        int xF= (int)Math.floor(xM);
+        int yF= (int)Math.floor(yM);
+        Point result = new Point(xF, yF);
+        return result;
+        //implementato round
+    }
+
+
+}
