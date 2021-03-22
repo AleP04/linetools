f3ed4b5a0eb4e75e28c0ed465fa2294d960a67bf
AleP04 <alessandropasqui90@gmail.com>
2021-03-22 12:17:31 +0100

    Removed Unnecessary comments

--- src/linetools/Line.java
+++ src/linetools/Line.java
@@ -47,8 +47,6 @@ public class Line {
     public Point intersection(Line s){
         double xM = (double)((s.intercept()-this.intercept())/(this.slope()-s.slope()));
         double yM = (double)(this.slope()*xM+this.intercept());
-//        double xM = (double)((q2 - q1) / (m1 - m2));
-//        double yM = (double)(m1 * xM + q1);
         int xF = (int)Math.round(xM);
         int yF = (int)Math.round(yM);
         Point result = new Point(xF, yF);
